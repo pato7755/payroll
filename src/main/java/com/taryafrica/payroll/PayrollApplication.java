60d86f92b893487ff4d15bc0999bc06ff159d243
Patrick Essiam <pat.essiam701@gmail.com>
2022-01-08 11:28:19 +0000

    Extend SpringBootServletInitializer in Application class

--- src/main/java/com/taryafrica/payroll/PayrollApplication.java
+++ src/main/java/com/taryafrica/payroll/PayrollApplication.java
@@ -2,12 +2,19 @@ package com.taryafrica.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class PayrollApplication {
+@SpringBootApplication(scanBasePackages = "com")
+public class PayrollApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PayrollApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PayrollApplication.class);
     }
 
 }
